22aaa71812bab9fb5b5181f58cca2acbdcea3f4f
pluto <1752774884@qq.com>
2024-12-17 17:46:39 +0800

    feature: 新增部分eslint规则配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
+const a = '12';
+const b = '1';
 </script>
 
 <template>
-  <div>App</div>
+    <div></div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// 引入公共样式
+import '@/style/reset.scss';
+import '@/style/global-theme.scss';
+
+createApp(App).mount('#app');
