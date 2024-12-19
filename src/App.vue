8f0ee87fee131193648a40e8f64a62a9584026a3
pluto <1752774884@qq.com>
2024-12-19 17:46:59 +0800

    feature: 新增element-ui-plus和加载动画

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <script setup>
-const a = '12';
-const b = '1';
+import { ElConfigProvider } from 'element-plus';
+import en from 'element-plus/es/locale/lang/en';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+// import plusEn from 'plus-pro-components/es/locale/lang/en';
+// import plusZhCn from 'plus-pro-components/es/locale/lang/zh-cn';
+import { computed } from 'vue';
+
+defineOptions({
+    name: 'App',
+});
+
+const localeOptions = {
+    zh: { ...zhCn },
+    en: { ...en },
+};
+const currentLocale = computed(() => {
+    return '';
+});
 </script>
 
 <template>
-    <div></div>
+    <el-config-provider :locale="currentLocale">
+        <router-view />
+    </el-config-provider>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { useElementPlus } from '@/plugins/elementPlus';
+
 // 引入公共样式
 import '@/style/reset.scss';
 import '@/style/global-theme.scss';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(useElementPlus);
+
+app.mount('#app');
